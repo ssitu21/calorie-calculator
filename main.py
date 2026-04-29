2bb07c11919fca909a2811a8a929fa93ab2a6137
ssitu21 <shawns097@deltalearns.ca>
2026-04-29 10:27:05 -0700

    inital setup
    pseudocode

---
+++ main.py
@@ -0,0 +1,50 @@
+# Calorie Calculator 
+
+
+# START PROGRAM
+
+# Create GUI window
+# Set window title and size
+
+# CREATE INPUT FIELDS
+
+# - Age input box
+# - Weight input box
+# - Height input box
+
+# CREATE DROPDOWN (Activity level)'
+
+# - Inactive
+# - Light
+# - Moderate
+# - Active
+
+# CREATE RADIO BUTTONS (Goal)
+
+# - Lose weight
+# - Maintain weight
+# - Gain weight
+
+# CREATE CALCULATE BUTTON
+
+# WHEN BUTTON CLICKED
+
+# - Get weight input
+# - Calculate base calories = weight × 22
+
+# IF goal = lose weight
+
+# subtract 300 calories
+
+# IF goal = gain weight
+
+# add 300 calories
+
+# APPLY ACTIVITY MULTIPLIER:
+# - Inactive → ×1.2
+# - Light → ×1.4
+# - Moderate → ×1.6
+# - Active → ×1.8
+
+# DISPLAY RESULT ON SCREEN
+
